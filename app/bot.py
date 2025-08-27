151b410fbbbf3b54f8e2c4b73b887bf8c9c66d27
djumanov <djumanovdev@gmail.com>
2025-08-27 09:04:53 +0500

    run bot

---
+++ app/bot.py
@@ -0,0 +1,20 @@
+from .config import config
+
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+)
+
+from . import handlers
+
+
+def run_bot() -> None:
+    updater = Updater(config.BOT_TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', handlers.start))
+
+    updater.start_polling()
+    updater.idle()
+
